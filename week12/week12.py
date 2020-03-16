21fff1f34aa161b28d6b2f3e802d0c05425290c6
Mathias <feldt.mathias@hotmail.com>
2020-03-16 08:56:14 +0100

    test

---
+++ week12/week12.py
@@ -0,0 +1,12 @@
+class A:
+    def __init__(self):
+        self.__name = 'Claus'
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if name in [str]:
+            self.__name = name
